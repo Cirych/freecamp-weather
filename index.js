544fa1d0bd3c117f2ef5d88fe3f7f9a3dc66e48d
Cirych <cir_reg@mail.ru>
2015-08-13 22:59:05 +0300

    Change bckgnd images source to GitHub

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //obj = {"ip":{"ip":"217.118.78.103","hostname":"No Hostname","city":"Saint Petersburg","region":"Saint Petersburg City","country":"RU","loc":"59.8944,30.2642","org":"AS16345 OJSC VimpelCom","postal":"190008"},"weather":{"coord":{"lon":30.26,"lat":59.89},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"cmc stations","main":{"temp":-29.61,"pressure":1013,"humidity":68,"temp_min":19,"temp_max":20.56},"wind":{"speed":8,"deg":280},"clouds":{"all":40},"dt":1439461160,"sys":{"type":1,"id":7267,"message":0.0056,"country":"RU","sunrise":1439431729,"sunset":1439488626},"id":498817,"name":"Saint Petersburg","cod":200}};
 //showWeather();
-var imgsrc = "";
+var imgsrc = "https://raw.githubusercontent.com/Cirych/freecamp-weather/master/img/";
 var obj = {};
 $.get("http://ipinfo.io", function(response) {
   obj.ip = response;
@@ -13,7 +13,7 @@ $.get("http://ipinfo.io", function(response) {
 function back(temp) {
 	var sign = (temp<0)?"-":""; console.log(sign);
 	temp=/\d/.exec(temp);
-	$('html').css("background", "url(img/"+sign+temp+".jpg) no-repeat rgba(0, 0, 0, 0.4)");
+	$('html').css("background", "url("+imgsrc+sign+temp+".jpg) no-repeat rgba(0, 0, 0, 0.4)");
 	$('html').css("background-size", "cover");
 };
 function cToF(type) {
